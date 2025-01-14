196733cd95fca094a681b49a143d76129fe75cf1
Florian Thubin <florianthubin@MacBook-Air-de-Florian.local>
2025-01-14 16:05:06 +0100

    ajout salle

--- src/Room/Room.java
+++ src/Room/Room.java
@@ -2,67 +2,16 @@ package Room;
 
 import java.util.Random;
 
-import Character.Player;
+public abstract class Room {
+    public abstract void enter();
 
-public class Room {
-    int number;
-    RoomType type;
-
-    public enum RoomType {
-        COMBAT, TREASURE, EMPTY, TRAP
-    }
-
-    public Room(int number, RoomType type) {
-        this.number = number;
-        this.type = type;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public RoomType getType() {
-        return type;
-    }
-
-    public void setType(RoomType type) {
-        this.type = type;
-    }
-
-    public static Room generateRandomRoom(int number) {
+    // Méthode statique pour générer une salle aléatoire
+    public static Room generateRandomRoom() {
         Random random = new Random();
-        RoomType type;
         if (random.nextBoolean()) {
-            type = RoomType.COMBAT;
+            return new CombatRoom();
         } else {
-            RoomType[] eventTypes = {RoomType.TREASURE, RoomType.EMPTY, RoomType.TRAP};
-            type = eventTypes[random.nextInt(eventTypes.length)];
-        }
-        return new Room(number, type);
-    }
-
-    public static class roomAction {
-    
-        public void roomEffect(Player player) {
-            player.setHealth(player.getHealth() - 20);
-        }
-
-        public void roomEmpty() {
-            return;
-        }
-
-        public void roomCombat(Player player) {
-            return;
-        }
-
-        public void roomTreasure(Player player) {
-            return;
+            return new EventRoom();
         }
     }
-
-
 }
