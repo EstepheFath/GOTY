b7c5c542ffccf8f6a2dece9f81b6565db6c771de
Florian Thubin <florianthubin@MacBook-Air-de-Florian.local>
2025-01-17 09:34:00 +0100

    e

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // Insert data into the database
-        // Update data in the database
-        MongoConnection.updateData("monRpg", "personnages", "flo", 15);
+        Game game = new Game();
+        game.start();
 
-        // Consult data from the database
-        MongoConnection.consultData("monRpg", "personnages");
-
-        // Close the connection
-        MongoConnection.close();
     }
 }
